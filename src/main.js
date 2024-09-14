7b12ff9b9a04804726722904d36c5b41cf8ad25a
chenchengjin <chenchengjin@sihoo.cn>
2024-09-14 17:45:20 +0800

    fix：引进路由

--- src/main.js
+++ src/main.js
@@ -3,10 +3,12 @@ import "./style.css";
 import App from "./App.vue";
 import ElementPlus from "element-plus";
 import "element-plus/dist/index.css";
+import router from "./router/index.js";
 
 // 创建应用实例
 const app = createApp(App);
 //配置使用Element plus
-app.use(ElementPlus)
+app.use(ElementPlus);
+app.use(router);
 // 挂载应用
 app.mount("#app", true);
