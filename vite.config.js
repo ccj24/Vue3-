529985b476b51cd5120a7ac8d37fe881085c7119
chenchengjin <chenchengjin@sihoo.cn>
2024-10-10 15:03:48 +0800

    fix:配置NetWor地址

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,19 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
 
 // 配置跨域
 export default defineConfig({
   plugins: [vue()],
   server: {
+    host: "0.0.0.0", //配置NetWor地址
     open: false,
     port: 3000,
     proxy: {
-      '/api': {
-        target: 'http://117.33.255.178:8082/zhifou-study',//后端地址
+      "/api": {
+        target: "http://117.33.255.178:8082/zhifou-study", //后端地址
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '')
-      }
-    }
-  }
-})
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
